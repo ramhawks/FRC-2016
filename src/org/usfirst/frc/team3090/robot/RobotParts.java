f2dd93f09a7484aa20b639ec06e0af6c6624ea7a
Winona Ramhawks <winonaramhawks@gmail.com>
2017-01-20 17:27:24 -0600

    Changed PWMs for lift motors

--- src/org/usfirst/frc/team3090/robot/RobotParts.java
+++ src/org/usfirst/frc/team3090/robot/RobotParts.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team3090.robot;
 import edu.wpi.first.wpilibj.Jaguar;
 
 public class RobotParts {
-	public static final Jaguar lift_motor_1 = new Jaguar(0);
-	public static final Jaguar lift_motor_2 = new Jaguar(1);
+	public static final Jaguar lift_motor_1 = new Jaguar(4);
+	public static final Jaguar lift_motor_2 = new Jaguar(5);
 
 }
